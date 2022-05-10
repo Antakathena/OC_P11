08cf0007cf16580c89dbf9260a1bd86e66fdc5f5
Antakathena <s.trebatius@gmail.com>
2022-05-10 09:33:19 +0200

    small refactor and correct to PEP8

---
+++ config.py
@@ -0,0 +1,7 @@
+# fichier où ranger les infos comme secret key ou autres ID à conserver
+
+# to generate a new secret key : import random, string
+# "".join([random.choice(string.printable) for _ in range(24)])
+# sinon site comme MiniWebTool
+
+# SECRET_KEY = "WjtjmZcug)IY`]v+-ebwC'[T"
---
+++ run.py
@@ -0,0 +1,5 @@
+import GUDLFTapp
+from GUDLFTapp import app
+
+if __name__ == "__main__":
+    app.run(debug=True)  # change when debug is done
